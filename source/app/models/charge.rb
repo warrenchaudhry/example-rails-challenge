659ffd62dd41fe5980f2d96d3bccd99951a8bcb0
Warren Chaudhry <warren.chaudhry@advitor.com>
2014-02-12 23:54:30 +0800

    Bootstrap

--- source/app/models/charge.rb
+++ source/app/models/charge.rb
@@ -2,8 +2,8 @@ class Charge < ActiveRecord::Base
 	
 	belongs_to :customer
 	
-	scope :successful, -> { where(paid: true, refunded: false) }
-	scope :failed, -> { where(paid: false) }
+	scope :successful, -> { where(paid: true, refunded: false)  }
+	scope :failed, -> { where(paid: false) }  
 	scope :disputed, -> { where(paid: true, refunded: true) }
 	
 end
--- source/app/models/customer.rb
+++ source/app/models/customer.rb
@@ -2,4 +2,9 @@ class Customer < ActiveRecord::Base
 	
 	has_many :charges, :dependent => :destroy
 	
+	
+  def formal_name
+    "#{last_name},  #{first_name}"
+  end
+  
 end
